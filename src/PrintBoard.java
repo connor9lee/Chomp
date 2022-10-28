b150746a83e6f26489ff2aa079f47106b8a3ca91
connor9lee <connor_lee24@milton.edu>
2022-10-28 11:14:47 -0400

    Finished print result boards

--- src/PrintBoard.java
+++ src/PrintBoard.java
@@ -15,41 +15,56 @@ public class PrintBoard {
                     board[1] = b;
                     board[2] = c;
                     //System.out.println(board[0] + ", " + board[1] + ", " + board[2]);
-                    System.out.println("board: " + board[0] + ", " + board[1] + ", " + board[2] + "; possible result board(s): " + "; best move: (" + "),(" + ")");
+                    System.out.println("The board is: " + board[0] + " " + board[1] + " " + board[2]);
+                    System.out.println("Possible result board(s): ");
                     possibleResult(a,b,c);
+                    System.out.println("Best move: (" + "),(" + ")");
+                    System.out.println("");
 
                 }
             }
         }
     }
     public void possibleResult (int a, int b, int c) {
-        for(int c2 = c; c2 <= b; c2--) {
+
+
+
+        for(int c2 = c-1; c2 >= 0; c2--) {
             int[] board2 = new int[10];
             board2[0] = a;
             board2[1] = b;
-            board2[3] = c2;
-            System.out.println(a + ", " + b + ", " + c2);
+            board2[2] = c2;
+            System.out.println(a + " " + b + " " + c2);
         }
-        for(int b2 = b; b2 <= a; b2--) {
+        for(int b2 = b-1; b2 >= 0; b2--) {
             int[] board3 = new int[10];
             board3[0] = a;
             board3[1] = b2;
-            board3[3] = 2;
-            System.out.println(a + ", " + b + ", " + c2);
+            board3[2] = c;
+            if (b2 < c){
+                System.out.println(a + " " + b2 + " " + b2);
             }
-        for(int a2 = a; a2 <= 3; a2--){
+            else {
+                System.out.println(a + " " + b2  + " " + c);
+            }
+
+        }
+        for(int a2 = a-1; a2 >= 1; a2--){
             int[] board2 = new int[10];
-            board2[0] = a;
+            board2[0] = a2;
             board2[1] = b;
-            board2[3] = c2;
-            System.out.println(a + ", " + b + ", " + c2);
-                }
+            board2[2] = c;
+            if (a2 < b && a2 < c ){
+                System.out.println(a2 + " " + a2 + " " + a2);
             }
+            else if (a2 < b){
+                System.out.println(a2 + " " + a2 + " " + c);
+            }
+            else {
+                System.out.println(a2 + " " + b + " " + c);
+            }
+
+
         }
     }
-    //System.out.println("a b c represents columns.");
-    //System.out.println("The possible combinations for a 3x3 are:");
-    //System.out.println("3 3 3, 3 3 2, 3 3 1, 3 2 2, 3 2 1, 3 1 1, 3 1 0, 3 3 0, 3 2 0, 3 0 0, ");
-    //System.out.println("2 2 2, 2 2 1, 2 2 0, 2 1 1, 2 1 0, 2 0 0,");
-    //System.out.println(" 1 1 1, 1 1 0, 1 0 0");
 }
